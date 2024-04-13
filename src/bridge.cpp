2f110ccd63dc395d9c7ae3d63aa071818b3a3fab
Beast <yungjunkim@icloud.com>
2024-04-13 16:55:57 -0400

    added support for tf_static in for the parameter bridge

--- src/bridge.cpp
+++ src/bridge.cpp
@@ -129,6 +129,15 @@ create_bidirectional_bridge(
     ros2_node->get_logger(), "create bidirectional bridge for topic %s",
     topic_name.c_str());
   BridgeHandles handles;
+  // added for supporting tf_static in parameter bridge 
+  auto ros2_publisher_qos = rclcpp::QoS(rclcpp::KeepLast(queue_size));
+  if (topic_name == "tf_static") {
+	  RCLCPP_INFO(ros2_node->get_logger(), "Setting QoS to keep_all msgs for topic /tf_static.");
+  	  ros2_publisher_qos.keep_all();
+          ros2_publisher_qos.transient_local();
+  }
+  // end //  
+
   handles.bridge1to2 = create_bridge_from_1_to_2(
     ros1_node, ros2_node,
     ros1_type_name, topic_name, queue_size, ros2_type_name, topic_name, queue_size);
@@ -153,6 +162,14 @@ create_bidirectional_bridge(
     ros2_node->get_logger(), "create bidirectional bridge for topic %s",
     topic_name.c_str());
   BridgeHandles handles;
+  // added for supporting tf_static in parameter bridge
+  auto ros2_publisher_qos = rclcpp::QoS(rclcpp::KeepLast(queue_size));
+  if (topic_name == "tf_static") {
+          RCLCPP_INFO(ros2_node->get_logger(), "Setting QoS to keep_all msgs for topic /tf_static.");
+          ros2_publisher_qos.keep_all();
+          ros2_publisher_qos.transient_local();
+  }
+  // end //
   handles.bridge1to2 = create_bridge_from_1_to_2(
     ros1_node, ros2_node,
     ros1_type_name, topic_name, queue_size, ros2_type_name, topic_name, publisher_qos);
